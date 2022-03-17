3c420aed1279f70344c2b57a17e8c5275cfff136
Mark Lyons <lyons340@gmail.com>
2022-03-16 20:46:16 -0400

    Added priority queue and updated readme.

---
+++ MarksPriorityQueue.cs
@@ -0,0 +1,96 @@
+﻿namespace Utilities.MarksPriorityQueue
+{
+    public class MarksPriorityQueue<T> where T : IComparable<T>
+    {
+        private readonly List<T> _pq = new List<T>();
+        public int Count => _pq.Count;
+        private readonly Comparer<T> InnerComparer;
+
+        public MarksPriorityQueue()
+        {
+            InnerComparer = Comparer<T>.Default;
+        }
+
+        public MarksPriorityQueue(Comparer<T> comparer)
+        {
+            InnerComparer = comparer;
+        }
+
+        public T Peek()
+        {
+            if (_pq.Count() == 0) throw new ArgumentException("Priority queue is empty. Cannot peek. Check count first.");
+            return _pq[0];
+        }
+
+        public void Enqueue(T item)
+        {
+            _pq.Add(item);
+            BubbleUp();
+        }
+
+        private void BubbleUp()
+        {
+            var childIndex = _pq.Count - 1;
+            while (childIndex > 0)
+            {
+                var parentIndex = (childIndex - 1) / 2;
+                if (InnerComparer.Compare(_pq[childIndex], _pq[parentIndex]) >= 0)
+                {
+                    break;
+                }
+                Swap(childIndex, parentIndex);
+                childIndex = parentIndex;
+            }
+        }
+
+        public T Dequeue()
+        {
+            var highestPrioritizedItem = _pq[0];
+
+            MoveLastItemToTheTop();
+            SinkDown();
+
+            return highestPrioritizedItem;
+        }
+
+        private void MoveLastItemToTheTop()
+        {
+            var lastIndex = _pq.Count - 1;
+            _pq[0] = _pq[lastIndex];
+            _pq.RemoveAt(lastIndex);
+        }
+
+        private void SinkDown()
+        {
+            var lastIndex = _pq.Count - 1;
+            var parentIndex = 0;
+
+            while (true)
+            {
+                var firstChildIndex = parentIndex * 2 + 1;
+                if (firstChildIndex > lastIndex)
+                {
+                    break;
+                }
+                var secondChildIndex = firstChildIndex + 1;
+                if (secondChildIndex <= lastIndex && InnerComparer.Compare(_pq[secondChildIndex], _pq[firstChildIndex]) < 0)
+                {
+                    firstChildIndex = secondChildIndex;
+                }
+                if (InnerComparer.Compare(_pq[parentIndex], _pq[firstChildIndex]) < 0)
+                {
+                    break;
+                }
+                Swap(parentIndex, firstChildIndex);
+                parentIndex = firstChildIndex;
+            }
+        }
+
+        private void Swap(int index1, int index2)
+        {
+            var tmp = _pq[index1];
+            _pq[index1] = _pq[index2];
+            _pq[index2] = tmp;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # CSharpPriorityQueueForInterviews
+
+[Priority Queues are available in .NET 6.0](https://docs.microsoft.com/en-us/dotnet/api/system.collections.generic.priorityqueue-2?view=net-6.0). However, some companies that use HackerRank/CoderPad/etc. may not have updated to .NET 6.0 just yet and you will be unable to use Priority Queues for your solutions in that case. I'm not sure what is involved in updating those platforms, but I did encounter this once as an interviewee and strangely enough, I did not have this issue on the same platform but with a different company. 
+
+This is a simple Priority Queue implementation that you can use. Most of this is from [this repository](), but I added support for custom comparers and a peek method. Here is an example of it being used as a max heap to get the K smallest elements in a list:
+
+```
+MarksPriorityQueue<int> maxHeap = new MarksPriorityQueue<int>(Comparer<int>.Create((x, y) => y - x));
+
+int[] testNums = new int[] { 8, 2, 3, 4, 9, 6, 7, 10 };
+int K = 3;
+
+for (int i = 0; i < K; i++)
+{
+    maxHeap.Enqueue(testNums[i]);
+}
+
+for (int i = K; i < testNums.Length; i++)
+{
+    if (testNums[i] < maxHeap.Peek())
+    {
+        maxHeap.Dequeue();
+        maxHeap.Enqueue(testNums[i]);
+    }
+}
+
+List<int> resultSet = new List<int>();
+for (int i = 0; i < K; i++)
+{
+    resultSet.Add(maxHeap.Dequeue());
+}
+
+Console.WriteLine(string.Join(", ", resultSet));
+```
